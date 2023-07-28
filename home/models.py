43ebf7fcfb9ddb2deb3322038812c828256ab953
anannya26 <anannyachamat269@gmail.com>
2023-07-29 04:27:18 +0530

    update

--- home/models.py
+++ home/models.py
@@ -15,3 +15,10 @@ class DOTComponents():
         self.replacement_due = replacement_due
         self.failure_mode = failure_mode
 
+class DOT_Systems():
+    def __init__(self,sr_no,systems,components,replacement_due,failure_mode) -> None:
+        self.sr_no = sr_no
+        self.systems = systems
+        self.components = components
+        self.replacement_due = replacement_due
+        self.failure_mode = failure_mode
