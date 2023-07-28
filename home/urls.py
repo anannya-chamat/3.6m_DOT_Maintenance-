a78eec48dcfaabc2283b02fcb7b1abfd21f1580e
anannya26 <anannyachamat269@gmail.com>
2023-07-29 04:27:38 +0530

    updated

--- home/urls.py
+++ home/urls.py
@@ -14,9 +14,16 @@ urlpatterns = [
         path('system_overview/ground-floor/gf_pnumatical', views.gf_pnumatical, name='gf_pnumatical'),
         path('system_overview/ground-floor/gf_hydraulic', views.gf_hydraulic, name='gf_hydraulic'),
         path('system_overview/ground-floor/gf_cooling', views.gf_cooling, name='gf_cooling'),
-     path('system_overview/ground-floor/gf_maintenance', views.gf_maintenance, name='gf_maintenance'),
+        path('system_overview/ground-floor/gf_maintenance', views.gf_maintenance, name='gf_maintenance'),
+        
      path('system_overview/7m/', views.seven_m, name='7m'),
+        path('system_overview/7m/7m_system/', views.seven_system, name='7m_system'),
+        path('system_overview/7m/7m_maintenance/', views.seven_maintenance, name='7m_maintenance'),
+        
      path('system_overview/11m/', views.eleven_m, name='11m'),
+        path('system_overview/11m/11m_system/', views.eleven_system, name='11m_system'),
+        path('system_overview/11m/11m_maintenance/', views.eleven_maintenance, name='11m_maintenance'),
+
      path('system_overview/dome/', views.dome, name='dome'),
      path('system_overview/ext_building/', views.ext_building, name='extbuilding'),
     path('maintenance_schedule/', views.maintenance_schedule, name='maintenance_schedule'),
