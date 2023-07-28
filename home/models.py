cdb2a813d41c790d1721758cb4e5e78bf6d58201
anannya26 <anannyachamat269@gmail.com>
2023-07-29 03:33:20 +0530

    models updated

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
-from django.db import models
 
-# Create your models here.
+class DOT_Maintenance():
+    def __init__(self,levels,units,failure_mode,maintenance) -> None:
+        self.levels = levels
+        self.units = units
+        self.failure_mode = failure_mode
+        self.maintenance = maintenance
+
+class DOTComponents():
+    def __init__(self,sr_no,systems,units,components,replacement_due,failure_mode) -> None:
+        self.sr_no = sr_no
+        self.systems = systems
+        self.units = units
+        self.components = components
+        self.replacement_due = replacement_due
+        self.failure_mode = failure_mode
+
