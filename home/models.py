b8729eed29cf012888d78754721595f3d3bf3c59
anannya26 <anannyachamat269@gmail.com>
2023-07-29 23:04:04 +0530

    models updated

--- home/models.py
+++ home/models.py
@@ -22,3 +22,18 @@ class DOT_Systems():
         self.components = components
         self.replacement_due = replacement_due
         self.failure_mode = failure_mode
+
+class MaintenanceDataEntry:
+    def __init__(self,system,task_detail,completion_date,responsible_user,additional_note):
+        self.system=system
+        self.task_detail=task_detail
+        self.completion_date=completion_date
+        self.responsible_user=responsible_user
+        self.additional_note=additional_note
+
+    def __str__(self):
+        return f"  System: {self.system}\n" \
+               f"  Task Detail: {self.task_detail}\n" \
+               f"  Completion Date: {self.completion_date}\n" \
+               f"  Responsible User: {self.responsible_user}\n" \
+               f"  Additional Note: {self.additional_note}"
