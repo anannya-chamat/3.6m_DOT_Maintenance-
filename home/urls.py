30b329852fba4c7546ae556d8bb0e1672e4219cd
anannya26 <anannyachamat269@gmail.com>
2023-07-30 16:26:26 +0530

    updated

--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
-# from home.views import extbuilding
 
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('login/', views.login, name='login'),
      path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
+   
     path('system_overview/', views.system_overview, name='system_overview'),
      path('system_overview/ground-floor/', views.ground_floor, name='ground_floor'),
         path('system_overview/ground-floor/gf_pnumatical', views.gf_pnumatical, name='gf_pnumatical'),
@@ -35,5 +36,6 @@ urlpatterns = [
 
     path('maintenance_schedule/', views.maintenance_schedule, name='maintenance_schedule'),
     path('maintenance_data_entry/', views.maintenance_data_entry, name='maintenance_data_entry'),
+    
     path('help_support/', views.help_support, name='help_support')
 ]
